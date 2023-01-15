1c213420327104fd2774650ac25ec0bb35de5487
Ryan <ryancooky6@gmail.com>
2023-01-15 15:13:34 -0500

    updated code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import fileupload from '../assets/fileupload.jpg';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import Textbox from './Textbox';
 import ImageUploader from './ImageUploader';
 
@@ -15,54 +15,41 @@ const text2 =  `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo es
 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum vel quibusdam aliquam cumque repellendus tempore tempora illo, reiciendis corrupti doloremque quia excepturi tenetur provident quae?\n
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, totam?`;
 
-//useState for textarea 1 and textarea 2 with three paragraphs of populated text
-    const [value1, setValue1] = useState(text1);
-    const [value2, setValue2] = useState(text2);
+//useState for textarea 1 and textarea 2 and storing/displaying the image
+    const [[value1, setValue1],[value2, setValue2],[img,setImg]] = [useState(text1,),useState(text2),useState(null)];
 
 //function for setting text in text area 1 and storing it in localStorage (to avoid refresh)
     const handleChange = (e, key, setter) => { 
         setter(e.target.value)
         localStorage.setItem(`value${key}`,(e.target.value));
       } 
-
-
+      
+//function that sets the image after once an image is uploaded
+const handleUpload = (e) => {
+    setImg(URL.createObjectURL(new Blob(e.target.files)));
+    localStorage.setItem('img', URL.createObjectURL(new Blob(e.target.files)));
+    document.getElementById("format").style.display = "none";
+    document.getElementById("fileimage").style.display = "none"; 
+    document.getElementById("reset").style.display = "block";   
+}
 //useEffect which receives text from localStorage when the page refreshes
     React.useEffect (() => {
-         const data1 = localStorage.getItem("value1");
-         const data2 = localStorage.getItem("value2");
+         const data = [localStorage.getItem("value1"),localStorage.getItem("value2")];
         //if no text is stored in localStorage don't set the text 
-        if(data1 || data2) {
-            setValue1(data1);
-            setValue2(data2);
+        if(data[0] || data[1]) {
+            setValue1(data[0]);
+            setValue2(data[1]);
         }
-    }, []);
-
-//useState and useRef for storing and displaying the image
-    const [img, setImg] = useState(null);
-  
-//function which 
-    const handleUpload = (e) => {
-        setImg(URL.createObjectURL(new Blob(e.target.files)));
-        localStorage.setItem('img', URL.createObjectURL(new Blob(e.target.files)));
-        document.getElementById("format").style.display = "none";
-        document.getElementById("fileimage").style.display = "none"; 
-        document.getElementById("reset").style.display = "block";   
-    }
-
-//useEffect which receives the image from localStorage and displays it when the page refreshes, hides any previous information and adds a reset button
-    React.useEffect (() => {
-        const image = (localStorage.getItem('img'));
-        //if no image is stored in localStorage, don't set the image
-        if (image){
-            setImg(image);
+        if (localStorage.getItem('img')){
+            setImg(localStorage.getItem('img'));
             //hides everything but the image and reset button
             document.getElementById("format").style.display = "none";
             document.getElementById("fileimage").style.display = "none"; 
             document.getElementById("reset").style.display = "block"; 
             document.getElementById("fileupload").style.padding = "0px 40px"; 
         }
-   });
-
+    });
+  
 //reset button which removes the image from localStorage
    const reset = () => {
     localStorage.removeItem('img');
@@ -78,5 +65,4 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, totam?`;
     </div>
   )
 }
-
 export default Main
